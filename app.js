4a1923e5ebdbe3566e81437ec7bc4ea29298dd94
aotatsu8 <70871669+aotatsu8@users.noreply.github.com>
2022-01-19 21:52:21 +0900

    Node.jsからMySQLへの接続

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const applicationlogger = require("./lib/log/applicationlogger.js");
 const accesslogger = require("./lib/log/accesslogger.js");
 const express = require("express");
 const favicon = require("serve-favicon");
+const { ADDRGETNETWORKPARAMS } = require("dns");
 const app = express();
 
 // Express settings
@@ -20,6 +21,38 @@ app.use(accesslogger());
 
 // Dynamic resource rooting
 app.use("/", require("./routes/index.js"));
+app.use("/test", async (req, res, next) => {
+    const { promisify } = require("util");
+    const path = require("path");
+    const { sql } = require("@garafu/mysql-fileloader")({root: path.join(__dirname,"./lib/databases/sql")});
+    const config = require("./config/mysql.config");
+    const mysql = require("mysql");
+    const con = mysql.createConnection({
+        host: config.HOST,
+        port: config.PORT,
+        user: config.USERNAME,
+        password: config.PASSWORD,
+        database: config.DATABASES,
+    });
+    const client = {
+        connect: promisify(con.connect).bind(con),
+        query: promisify(con.query).bind(con),
+        end: promisify(con.end).bind(con)
+    };
+    var data;
+
+    try{
+        await client.connect();
+        data = await client.query(await sql("SELECT_SHOP_BASIC_BY_ID"));
+        console.log(data);
+    }catch(err){
+        next(err);
+    }finally{
+        await client.end();
+    }
+
+    res.end("OK");
+});
 
 // Set application log
 app.use(applicationlogger());
