bf5beb5e8828e49a55d17116ad1ce750118e0405
aotatsu8 <70871669+aotatsu8@users.noreply.github.com>
2022-01-20 15:04:27 +0900

    DB接続機能のモジュール化

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const applicationlogger = require("./lib/log/applicationlogger.js");
 const accesslogger = require("./lib/log/accesslogger.js");
 const express = require("express");
 const favicon = require("serve-favicon");
-const { ADDRGETNETWORKPARAMS } = require("dns");
 const app = express();
 
 // Express settings
@@ -22,33 +21,17 @@ app.use(accesslogger());
 // Dynamic resource rooting
 app.use("/", require("./routes/index.js"));
 app.use("/test", async (req, res, next) => {
-    const { promisify } = require("util");
-    const path = require("path");
-    const { sql } = require("@garafu/mysql-fileloader")({root: path.join(__dirname,"./lib/databases/sql")});
-    const config = require("./config/mysql.config");
-    const mysql = require("mysql");
-    const con = mysql.createConnection({
-        host: config.HOST,
-        port: config.PORT,
-        user: config.USERNAME,
-        password: config.PASSWORD,
-        database: config.DATABASES,
-    });
-    const client = {
-        connect: promisify(con.connect).bind(con),
-        query: promisify(con.query).bind(con),
-        end: promisify(con.end).bind(con)
-    };
+    const { MySQLClient, sql } = require("./lib/databases/client.js");
     var data;
 
     try{
-        await client.connect();
-        data = await client.query(await sql("SELECT_SHOP_BASIC_BY_ID"));
+        await MySQLClient.connect();
+        data = await MySQLClient.query(await sql("SELECT_SHOP_BASIC_BY_ID"));
         console.log(data);
     }catch(err){
         next(err);
     }finally{
-        await client.end();
+        await MySQLClient.end();
     }
 
     res.end("OK");
