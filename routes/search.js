a736300ca5062be541f8fb9961c133f3ba872134
aotatsu8 <70871669+aotatsu8@users.noreply.github.com>
2022-01-25 18:54:09 +0900

    店舗検索のデータ取得部分の作成

---
+++ routes/search.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const { MySQLClient, sql } = require("../lib/databases/client.js");
+const MAX_ITEMS = 5;
+
+router.get("/", async (req, res, next)=>{
+    var keyword = req.query.keyword || "";
+    var results;
+
+    try{
+        if(keyword){
+            results = await MySQLClient.executeQuery(
+                await sql("SELECT_SHOP_LIST_BY_NAME"),
+                [`%${keyword}%`, MAX_ITEMS]
+            );
+        }else{
+            results = await MySQLClient.executeQuery(
+                await sql("SELECT_SHOP_HIGHT_SCORE_LIST"),
+                [MAX_ITEMS]
+            );
+        }
+
+        res.render("./search/list.ejs", { results });
+    }catch(err){
+        next(err);
+    }
+
+});
+
+module.exports = router;
