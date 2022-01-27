f1f954e8be66851be603eeb8fa9f51aab94428bc
aotatsu8 <70871669+aotatsu8@users.noreply.github.com>
2022-01-27 16:19:38 +0900

    作成したトランザクションの動作確認

--- app.js
+++ app.js
@@ -26,6 +26,23 @@ app.use("/public", express.static(path.join(__dirname, "/public")));
 app.use(accesslogger());
 
 // Dynamic resource rooting
+app.get("/test", async (req, res, next) => {
+    const { MySQLClient } = require("./lib/databases/client.js");
+    var tran;
+    try{
+        tran = await MySQLClient.beginTransaction();
+        await tran.executeQuery(
+            "UPDATE t_shop SET score=? WHERE id=?",
+            [4.11, 1]
+        );
+        throw new Error("Test exception");
+        await tran.commit();
+        res.end("OK");
+    }catch(err){
+        await tran.rollback();
+        next(err);
+    }
+});
 app.use("/search", require("./routes/search.js"));
 app.use("/shops", require("./routes/shops.js"));
 app.use("/", require("./routes/index.js"));
